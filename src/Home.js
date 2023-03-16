ec15c970a3865a94f478f9862d8bba58339ef3e5
Sean <napolisc10@gmail.com>
2023-03-16 14:00:01 -0600

    Created basic home component.

---
+++ src/Home.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function Home() {
+return (
+     <div>
+        <h1>Welcome to the Random Workout Generator by Delta-Five Performance!</h1>
+        <p>This application creates a random, full-body workout for you to try at your leisure.</p>
+        <img src="https://lh3.googleusercontent.com/d5c1FUE8Bk3F5m6XFhyXtNvXBGhsM5V2_1WKmjh00LgE0BX2GGXQ439Tt78LUxMdAWSQ0IhU6fbitzOJ6ClGL54SJOCETwqnxPsnsrS8yftecg4VWyFnnhkz2T7IyoTPKFPcCC-w3xn8-GXBlJSIhTCPXBYn-o0HOkZm3WKVyHK-W9CiAvxo448-eE4kbzywvoenDBb2oAte4bb-vEuZlwhcnYIFKhCoPkf9Wy5Gb-LacsPkN571-1CUfFxEP3rhUa93IawEUulJzIdTk43yhYgP4-9OX1Ogan0KRIMHc5eenRfit29oUGKXY9UIf7eVkm837SjwLJe8xjq4IJedMstJiUCQU9QW83ZTdtusgZVOjdkBE2LXHPhkhoxHOOumsfLgEND4PEjoHgIQKKvPS3WXBUqcqVodHHQaxCeb7PoQ7ysqnwDqCfxFu0rnsRlhfPSWB2imPQQBPlbNmDBSZh4LdrW-EDAXRjDXfHTTlQs5DxHTCMqsPfpgKANZxWw6oW8mbWMQxpji4BLN5u5JxjHjekcr3QouT44fi5JD03Td0YSN7kHnhdWPe7bzRX1KpqP3sMKjNw_IthfOA9HAOvgqtFt7gWxAHtXcfTk-K7_MQsVF8oWA0V2rY1VfUYd02rcB6rai1jA2KEvw-BmiRKkF-Ea2MeJOA6AghLAVGwnEUYeZEwXFwKLrjbYzA1F0vkvGmDrSL7NK4bnl0G7yvPu8pksl-6qbFDhcMJ6Qf7em78Kq35ZeWDAMpTonc13LpgxagAMDq_nG4rmqYQRlldqXQaMaDLmkAEdpf-e63gJE7e2l71LNwUeeNP-2egLiXDPrkM51acZdkbMCIQPjC_Jxj_OfWSGP1r5PqDpJm8hzAMh8rrRojDpjpgkd2vjZMfDhUqgYIX6ITAgsIYuNSZF5Gw8aiVr-p3eeX5rdDJ7Y_BLA=s1484-no?authuser=0" alt="Delta-Five Logo" id="DeltaFiveLogo"/>
+        
+    </div>
+    );
+    }
+ 
+export default Home;
