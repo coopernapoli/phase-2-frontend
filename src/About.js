0a13ec2a25d2ce83f02979f97544ede7a042382f
Sean <napolisc10@gmail.com>
2023-03-16 13:57:01 -0600

    Created basic framework.

---
+++ src/About.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function About() {
+return (
+     <div id="About">
+        <h1>About</h1>
+        <p> Delta-Five Performance primarily serves individuals seeking to break through a plateau in their training. We specialize, but are not limited too, helping prepare tactical athletes for selection courses and building specialized strength for combat sport athletes.
+        </p>
+        
+    </div>
+    );
+    }
+ 
+export default About;
