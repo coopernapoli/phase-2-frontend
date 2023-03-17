f2c291b38e03926fdf70e5af851bb69ff3ddf3db
Sean <napolisc10@gmail.com>
2023-03-17 15:15:15 -0600

    Refactored so as to add client side routes via the Navbar.

--- src/App.js
+++ src/App.js
@@ -1,16 +1,22 @@
-import logo from './logo.svg';
 import './App.css';
 import Home from './Home'
 import About from './About'
 import Contact from './Contact';
+import { Switch, Route, BrowserRouter } from 'react-router-dom';
+import Navbar from './Link';
 
 function App() {
   return (
+    <BrowserRouter>
     <div className="App">
-      <Home/>
-      <About/>
-      <Contact/>
+      <Navbar />
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route path="/about" component={About} />
+        <Route path="/contact" component={Contact} />
+      </Switch>
     </div>
+  </BrowserRouter>
   );
 }
 
