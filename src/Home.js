07660ae0ab1f3b17ff8a7cece9dd8c5131569097
Sean <napolisc10@gmail.com>
2023-03-21 17:36:51 -0600

    Double checking for end of day.

--- src/Home.js
+++ src/Home.js
@@ -2,10 +2,10 @@ import React from 'react';
 
 function Home() {
 return (
-     <div>
-        <h1>Welcome to the Random Workout Generator by Delta-Five Performance!</h1>
+     <div id='home'>
+        <h1 id='headerOne'>Welcome to the Random Workout Generator by Delta-Five Performance!</h1>
         <p>This application creates a random, full-body workout for you to try at your leisure.</p>
-        <img src="https://lh3.googleusercontent.com/d5c1FUE8Bk3F5m6XFhyXtNvXBGhsM5V2_1WKmjh00LgE0BX2GGXQ439Tt78LUxMdAWSQ0IhU6fbitzOJ6ClGL54SJOCETwqnxPsnsrS8yftecg4VWyFnnhkz2T7IyoTPKFPcCC-w3xn8-GXBlJSIhTCPXBYn-o0HOkZm3WKVyHK-W9CiAvxo448-eE4kbzywvoenDBb2oAte4bb-vEuZlwhcnYIFKhCoPkf9Wy5Gb-LacsPkN571-1CUfFxEP3rhUa93IawEUulJzIdTk43yhYgP4-9OX1Ogan0KRIMHc5eenRfit29oUGKXY9UIf7eVkm837SjwLJe8xjq4IJedMstJiUCQU9QW83ZTdtusgZVOjdkBE2LXHPhkhoxHOOumsfLgEND4PEjoHgIQKKvPS3WXBUqcqVodHHQaxCeb7PoQ7ysqnwDqCfxFu0rnsRlhfPSWB2imPQQBPlbNmDBSZh4LdrW-EDAXRjDXfHTTlQs5DxHTCMqsPfpgKANZxWw6oW8mbWMQxpji4BLN5u5JxjHjekcr3QouT44fi5JD03Td0YSN7kHnhdWPe7bzRX1KpqP3sMKjNw_IthfOA9HAOvgqtFt7gWxAHtXcfTk-K7_MQsVF8oWA0V2rY1VfUYd02rcB6rai1jA2KEvw-BmiRKkF-Ea2MeJOA6AghLAVGwnEUYeZEwXFwKLrjbYzA1F0vkvGmDrSL7NK4bnl0G7yvPu8pksl-6qbFDhcMJ6Qf7em78Kq35ZeWDAMpTonc13LpgxagAMDq_nG4rmqYQRlldqXQaMaDLmkAEdpf-e63gJE7e2l71LNwUeeNP-2egLiXDPrkM51acZdkbMCIQPjC_Jxj_OfWSGP1r5PqDpJm8hzAMh8rrRojDpjpgkd2vjZMfDhUqgYIX6ITAgsIYuNSZF5Gw8aiVr-p3eeX5rdDJ7Y_BLA=s1484-no?authuser=0" alt="Delta-Five Logo" id="DeltaFiveLogo"/>
+        <img src="https://lh3.googleusercontent.com/5QVr5T5hSMsduDjJxoUzoa5aPHrvoNiik-YEuXZ6Dpp3okWIzxScpCkWVX1iywKOxWwlTGyn8fdS9cnznrQWG3zJm-TXm_YchryegMcs8VoPwkPhbPQyFuN9bgJEn2MYTpn9c3S8shkhKE131L0N0_VFb5h2AYyHy13YriIA8Mpv0Vc45-Y5zrzKcHF5BylA12a6p5EpBBNVybjERAQqpDSjSYQg028UBhnu8upNvCjHddgZ3vLb3jNh87rsjQ5SUJximUwmQgCCdTFyUcElWisH5auF50R5IsLGCPTaha97UYtbEHI_n3T-VszE4B0EW7wt2v1Ha1pU7T6BzDiuCLXOvEVrCJV9DXKTNsMq-Dws9wWk81eYnHS1AWxkbQFi-SH3_V2NFDK3T1e2fcaLrHfLqpcbAepP7pJUuvjGFvBXi55Pxc-VW8W-YdHiK9zdKr5hBaB6kYZp-6gRlYahqudvPh8hV0jLSr2GTAkpfeUyetGCmADza6nTG-g70BOgjq7Ub0Ow0UAaTgZk2vv9XbQBc4ie2cNyklfH8Tb0RWCA0zAbovxrGnkrWbQNO6c_1LnKX8lWvlJUSpXRkI1tL-yHQT97zY_o8YdN_m_4n73HUpDuBaJsL7H1KrS9kzfhXKvQCrKbiBF9WVicba0zQPPGmydsl6kqWZYbiDTgpQX9WTKIDhYGy7P1LZkVXMNCznyVqyMhVhL8P_SCDS3cGbmXH0Ws5PgKWLaMMgzaGp_fDUIjaM-EAAVBDxrp0YlDNLg0DJJH2Rk9t2xuXaIHOyQU-jDOerAakv1r-eBFoB-j_J_5FwzqBO5T8M9qg3ulet4xQyYEsM0R3InZNdxdtmviEcTSEFT7DVMiC3ZibHIzRNypKigfS1zZuW27PdaPf4vT2Vupj1KggLHt3pLXWFRT5eDcVncov-1jRRJsG0zZ6sQ9=s1484-no?authuser=0" alt="Delta-Five Logo" id="DeltaFiveLogo"/>
         
     </div>
     );
