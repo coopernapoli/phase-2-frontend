ebf0df4760f8c512c1eb902e37a579d0a72732dd
Sean <napolisc10@gmail.com>
2023-03-22 16:16:00 -0600

    Added some extra wording.

--- src/About.js
+++ src/About.js
@@ -4,7 +4,7 @@ function About() {
 return (
      <div id="About">
         <h1>About</h1>
-        <p> Delta-Five Performance primarily serves individuals seeking to break through a plateau in their training. We specialize, but are not limited too, helping prepare tactical athletes for selection courses and building specialized strength for combat sport athletes.
+        <p> Delta-Five Performance primarily serves individuals seeking to break through a plateau in their training. We specialize, but are not limited too, helping prepare tactical athletes for selection courses and building specialized strength for combat sport athletes.  The more we train, the more we realize that athletes of all levels require better basic function of all of their biological systems.  Any athlete will improve exponentially using our dynamic systems based approach.
         </p>
         
     </div>
